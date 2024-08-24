8d6b4c8bfc39baf73f1b5c2312ced61854206168
Giant <gianalharits232@gmail.com>
2024-08-24 21:14:30 +0800

    feat: 404 page

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -2,9 +2,17 @@
 export default function Information() {
   return (
     <>
-      <div className="py-20">
-        <div className="container md:px-10">
-          <h1>Hello</h1>
+      <div className="h-screen container grid gap-4 justify-items-center place-content-center">
+        <img
+          src="/coming-soon.svg"
+          alt="coming-soon"
+          width={ 240 }
+          height={ 240 }
+        />
+        <div className="flex flex-col gap-2 justify-center text-center">
+          <h1 className="text-4xl font-bold text-[#1D2739]">Coming Soon!</h1>
+          <p className="text-lg font-medium text-[#475367]">We are curently working on this feature and will launce
+            soon</p>
         </div>
       </div>
     </>
